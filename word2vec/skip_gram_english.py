32c5d7379571723b7208cab311edebd75a6c39b7
loyal <furiedboy@gmail.com>
2018-09-13 17:17:25 +0800

    update

--- word2vec/skip_gram_english.py
+++ word2vec/skip_gram_english.py
@@ -80,3 +80,103 @@ prob_drop = { w: 1 - np.sqrt(t/word_freqs(w)) for w in int_word_counts }
 train_words = [ w for w in int_words if prob_drop[w] < threshold ]
 
 print(len(train_words))
+
+
+def get_targets(words, idx, window_size=5):
+    target_window = np.random.randint(1, window_size + 1)
+    start_point = idx - target_window if (idx - target_window) > 0 else 0
+    end_point = idx + target_window
+    targets = set(words[start_point: idx] + words[idx+1: end_point+1])
+    return list(targets)
+
+def get_batches(words, batch_size, window_size=5):
+    pass
+
+
+inputs = tf.placholder(tf.int32, shape=[None], name='inputs')
+outputs = tf.placeholder(tf.int32, shape=[None, None], name='labels')
+embedding = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1, 1))
+embed = tf.nn.embedding_lookup(embedding, inputs)
+
+n_sampled = 100
+softmax_w = tf.Variable(tf.truncated_normal([vocab_size, embedding_size], stddev=0.1))
+softmax_b = tf.Variable(tf.zeros(vocab_sieze))
+
+loss = tf.nn.sampled_softmax_loss(softmax_w, softmax_b, labels, embed, n_sampled, vocab_size)
+cost = tf.reduce_mean(loss)
+optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+train_grapg = tf.Graph()
+with train_graph.as_default():
+    valid_size = 7
+    valid_window = 100
+    valid_examples = np.array(random.sample(range(valid_window), valid_sieze//2))
+    valid_examples = np.append(valid_examples, random.sample(range(1000, 1000+valid_window), valid_size//2))
+
+    valid_examples = [vocab_to_int['word'],
+                      vocab_to_int['ppt'],
+                      vocab_to_int['熟悉'],
+                      vocab_to_int['java'],
+                      vocab_to_int['能力'],
+                      vocab_to_int['逻辑思维'],
+                      vocab_to_int['了解']]
+	valid_size = len(valid_examples)
+	valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+
+	norm = tf.sqrt(tf.reduce_sum(tf.square(embedding), 1, keep_dims=True))
+	normalized_embedding = embedding / norm
+	valid_embedding = tf.nn.embedding_lookup(normalized_embedding, valid_dataset)
+	similarity = tf.matmul(valid_embedding, tf.transpose(normalized_embedding))
+
+
+epochsepochs  ==  1010  # 迭代轮数# 迭代轮数
+ batch_sizebatch_si  = 1000 # batch大小
+window_size = 10 # 窗口大小
+
+with train_graph.as_default():
+    saver = tf.train.Saver() # 文件存储
+
+with tf.Session(graph=train_graph) as sess:
+    iteration = 1
+    loss = 0
+    sess.run(tf.global_variables_initializer())
+
+    for e in range(1, epochs+1):
+        batches = get_batches(train_words, batch_size, window_size)
+        start = time.time()
+        # 
+        for x, y in batches:
+
+            feed = {inputs: x,
+                    labels: np.array(y)[:, None]}
+            train_loss, _ = sess.run([cost, optimizer], feed_dict=feed)
+
+            loss += train_loss
+
+            if iteration % 100 == 0:
+                end = time.time()
+                print("Epoch {}/{}".format(e, epochs),
+                      "Iteration: {}".format(iteration),
+                      "Avg. Training loss: {:.4f}".format(loss/100),
+                      "{:.4f} sec/batch".format((end-start)/100))
+                loss = 0
+                start = time.time()
+
+            # 计算相似的词
+            if iteration % 1000 == 0:
+                # 计算similarity
+                sim = similarity.eval()
+                for i in range(valid_size):
+                    valid_word = int_to_vocab[valid_examples[i]]
+                    top_k = 8 # 取最相似单词的前8个
+                    nearest = (-sim[i, :]).argsort()[1:top_k+1]
+                    log = 'Nearest to [%s]:' % valid_word
+                    for k in range(top_k):
+                        close_word = int_to_vocab[nearest[k]]
+                        log = '%s %s,' % (log, close_word)
+                    print(log)
+
+            iteration += 1
+
+    save_path = saver.save(sess, "checkpoints/text8.ckpt")
+    embed_mat = sess.run(normalized_embedding)
